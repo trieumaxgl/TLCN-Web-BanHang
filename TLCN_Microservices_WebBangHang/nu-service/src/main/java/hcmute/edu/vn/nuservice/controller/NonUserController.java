389f68ffbd868211ae88a1e0a7cc7a4aceb93e8d
trieumax <33923844+trieumax@users.noreply.github.com>
2018-12-16 14:13:35 +0700

    update nu-service/src/main/java/hcmute/edu/vn/nuservice/controller/NonUserController.java

--- TLCN_Microservices_WebBangHang/nu-service/src/main/java/hcmute/edu/vn/nuservice/controller/NonUserController.java
+++ TLCN_Microservices_WebBangHang/nu-service/src/main/java/hcmute/edu/vn/nuservice/controller/NonUserController.java
@@ -18,6 +18,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/v1/nuser/")
+@CrossOrigin
 public class NonUserController {
     @Autowired
     private UserService userService;
